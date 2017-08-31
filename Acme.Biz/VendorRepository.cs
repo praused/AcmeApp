c312d74d39dc6c60934dec8a8e6d4385ddae8023
David Prause <praused@hotmail.com>
2017-08-31 10:56:57 -0400

    6 Generic Dictionaries, Iteratin Through

--- Acme.Biz/VendorRepository.cs
+++ Acme.Biz/VendorRepository.cs
@@ -49,20 +49,41 @@ namespace Acme.Biz
                 { "ABC Corp", new Vendor() { VendorId = 5, CompanyName = "ABC Corp", Email = "abc@abc.com" }},
                 { "XYZ Inc", new Vendor() { VendorId = 8, CompanyName = "XYZ Inc", Email = "xyz@xyz.com" }}
             };
-              
-            Console.WriteLine(vendors);
 
-            //Looks up the vendor twice
-            if (vendors.ContainsKey("XYZ Inc"))
-            { 
-                Console.WriteLine(vendors["XYZ Inc"]);
-            }
-            //Looks up the vendor only once
-            Vendor vendor;
-            if(vendors.TryGetValue("XYZ Inc", out vendor))
+            //Console.WriteLine(vendors);
+
+            ////Iteration by Keys
+            //foreach (var companyName in vendors.Keys)
+            //{
+            //    Console.WriteLine(companyName);
+            //    Console.WriteLine(vendors[companyName]);
+            //}
+
+            ////Iteration by Values
+            //foreach (var vendor in vendors.Values)
+            //{
+            //    Console.WriteLine(vendor);
+            //}
+
+            //Iteration by Element (key-value pairs) -- avoid because it is slower than iterating by keys or values
+            foreach (var element in vendors)
             {
-                Console.WriteLine(vendor);
+                var vendor = element.Value;
+                var key = element.Key;
+                Console.WriteLine($"Key: {key}, Value: {vendor}");
             }
+
+            ////Looks up the vendor twice
+            //if (vendors.ContainsKey("XYZ"))
+            //{ 
+            //    Console.WriteLine(vendors["XYZ"]);
+            //}
+            ////Looks up the vendor only once
+            //Vendor vendor;
+            //if(vendors.TryGetValue("XYZ", out vendor))
+            //{
+            //    Console.WriteLine(vendor);
+            //}
 
             return vendors;
         }
