8dfc84203d8d62b36afaf1d75cc938b6d57b3ca1
David Prause <praused@hotmail.com>
2017-08-31 09:33:41 -0400

    6 Generic Dictionaries, Collection Initializers

--- Acme.Biz/Product.cs
+++ Acme.Biz/Product.cs
@@ -16,6 +16,8 @@ namespace Acme.Biz
         #region Constructors
         public Product()
         {
+
+            #region Generic List
             //var colorOptions = new List<string>();
             //colorOptions.Add("Red");
             //colorOptions.Add("Espresso");
@@ -27,7 +29,24 @@ namespace Acme.Biz
             var colorOptions = new List<string>() { "Red", "Espresso", "White", "Navy" };
 
             Console.WriteLine(colorOptions);
+            #endregion
+
+            //var states = new Dictionary<string, string>();
+            //states.Add("CA", "California");
+            //states.Add("WA", "Washington");
+            //states.Add("NY", "New York");
+
+            var states = new Dictionary<string, string>()
+            {
+                { "CA", "California" },
+                { "WA", "Washington"},
+                { "NY", "New York" },
+            };
+
+            Console.WriteLine(states);
+
         }
+
         public Product(int productId,
                         string productName,
                         string description) : this()
