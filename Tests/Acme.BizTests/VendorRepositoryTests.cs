fef750b829c0dc997d911e49bb61fcc8b52edc7e
David Prause <praused@hotmail.com>
2017-09-04 10:54:44 -0400

    8 LINQ, Query Syntax

--- Tests/Acme.BizTests/VendorRepositoryTests.cs
+++ Tests/Acme.BizTests/VendorRepositoryTests.cs
@@ -95,5 +95,28 @@ namespace Acme.Biz.Tests
             CollectionAssert.AreEqual(expected, actual);
         }
 
+        [TestMethod()]
+        public void RetrieveAllTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = new List<Vendor>()
+            {
+                new Vendor() { VendorId = 22, CompanyName = "Amalgamated Toys", Email = "a@abc.com" },
+                new Vendor() { VendorId = 35, CompanyName = "Car Toys", Email = "car@abc.com" },
+                new Vendor() { VendorId = 28, CompanyName = "Toy Blocks Inc", Email = "blocks@abc.com" },                
+                new Vendor() { VendorId = 42, CompanyName = "Toys for Fun", Email = "fun@abc.com" },
+            };
+
+            //Act
+            var vendors = repository.RetrieveAll();
+            var vendorQuery = from v in vendors
+                              where v.CompanyName.Contains("Toy")
+                              orderby v.CompanyName
+                              select v;
+
+            //Assert
+            CollectionAssert.AreEqual(expected, vendorQuery.ToList());
+        }
     }
 }
